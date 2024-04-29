c385713630843b1f71870b0fb7129a1fe09c46ba
collinturbert <collinturbert@gmail.com>
2024-04-29 15:46:04 -0600

    April Update

--- VESL Client Database/ClientDatabase/ClientDatabase/settings.py
+++ VESL Client Database/ClientDatabase/ClientDatabase/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qp*_445)0z3nldb^__j=qh9pnwviaaum$4tzqop#7g218v@k7f"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
